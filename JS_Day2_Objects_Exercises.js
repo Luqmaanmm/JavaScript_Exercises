d7c6bbd1c7b276f642b5ed73a5c6797d57518601
Luqmaanmm <94450985+Luqmaanmm@users.noreply.github.com>
2021-11-28 21:39:00 +0400

    Initial Commit

---
+++ JS_Day2_Objects_Exercises.js
@@ -0,0 +1,16 @@
+//Exercise 1
+let person = {
+  username: "luqmaanmm", // QUESTION 1
+  password: "123456",
+};
+console.log(person);
+
+
+let database = [person]; // QUESTION 2
+console.log(database);
+
+
+let newsfeed = [[person.username, person.timeline],
+                [person.username, person.timeline],
+                [person.username, person.timeline]];
+console.log(newsfeed);
